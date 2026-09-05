0c483bea068cf2414aa7e2cc9cb7ef1d79c60275
kgmhz6hkyn-sudo <kgmhz6hkyn@privaterelay.appleid.com>
2026-09-05 20:22:16 +0200

    Refresh cache for Poiesis curation layer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='poiesis-v7';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./core.js','./daily.js','./links-fix.js','./explore-ui.js'];
+const CACHE='poiesis-v8';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./core.js','./daily.js','./links-fix.js','./explore-ui.js','./curation.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
