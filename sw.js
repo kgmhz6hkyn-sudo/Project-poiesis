591ab2ba69f0010c1b88c49b5a22e727e347d2f4
kgmhz6hkyn-sudo <kgmhz6hkyn@privaterelay.appleid.com>
2026-09-05 20:06:31 +0200

    Refresh Poiesis cache for Explore filters

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='poiesis-v4';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./assets/daily-art.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const CACHE='poiesis-v6';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./core.js','./patch.js','./explore-ui.js','./daily.js'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
