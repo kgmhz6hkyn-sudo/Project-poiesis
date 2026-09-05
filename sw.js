8c43cd36381170fc36e96207775a639739e0be23
kgmhz6hkyn-sudo <kgmhz6hkyn@privaterelay.appleid.com>
2026-09-05 20:10:03 +0200

    Fix service worker assets and refresh Explore update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='poiesis-v6';
-const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./core.js','./patch.js','./explore-ui.js','./daily.js'];
+const CACHE='poiesis-v7';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./core.js','./daily.js','./links-fix.js','./explore-ui.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
